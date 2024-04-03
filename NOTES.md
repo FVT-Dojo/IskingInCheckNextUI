a381e20179ee45386a870748771313ca18d985bc
Arthur van Aarssen <arthur.vanaarssen@ns.nl>
2024-04-03 12:00:06 +0200

    build: first E2E test practically finished

--- NOTES.md
+++ NOTES.md
@@ -10,3 +10,6 @@ https://nextjs.org/docs/pages/building-your-application/testing/playwright
 🥫 1:
 - 🙋‍♂️ Create end to end test
 - Add submodule with contract repo
+
+Another better documentation using the same structure that we do:
+https://www.genui.com/resources/getting-started-with-bdd-using-cucumber-io
--- tests/e2e/steps/iskingincheck.steps.ts
+++ tests/e2e/steps/iskingincheck.steps.ts
@@ -1,11 +1,12 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 
-import { Page, chromium, expect } from "@playwright/test";
+import { Locator, Page, chromium, expect } from "@playwright/test";
 
 let page: Page;
+let chessboard: Locator;
 
 Given('the user is in the UI', async () => {
-    const browser = await chromium.launch({ headless: false });
+    const browser = await chromium.launch();
 
     const context = await browser.newContext();
 
@@ -15,26 +16,29 @@ Given('the user is in the UI', async () => {
   });
 
 When('the user clicks the start game button', async () => {
-  await page.locator("[data-id=start-game-button]").click();
+  await page.locator('[data-id="start-game-button"]').click();
 });
 
 Then('the user sees a chessboard displayed on the screen', async () => {
-  const chessboard = await page.locator(`[id="chessboard"]`);
+  chessboard = await page.locator('[data-id="chessboard"]');
 
-  expect(chessboard).toBeDefined();
+  expect(chessboard).toBeVisible();
 });
 
-Then('the king is placed at E1', function () {
-// Write code here that turns the phrase above into concrete actions
-return true;
+Then('the king is placed at E1', async () => {
+  const squareE1 = await chessboard.locator('[data-id="E1"]');
+
+  expect(squareE1.textContent()).toBe('K');
 });
 
-Then('the rook is placed at A5', function () {
-// Write code here that turns the phrase above into concrete actions
-return true;
+Then('the rook is placed at A5', async () => {
+  const squareA5 = await chessboard.locator('[data-id="A5"]');
+
+  expect(squareA5.textContent()).toBe('R');
 });
 
-Then('a label is shown that indicates that the king is not in check', function () {
-// Write code here that turns the phrase above into concrete actions
-return true;
+Then('a label is shown that indicates that the king is not in check', async () => {
+  const isInCheckTextVisible = await page.isVisible('text="The king is in check"');
+
+  expect(isInCheckTextVisible).toBeTruthy();
 });
